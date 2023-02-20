7fc4d615522345df2301faf16a42453a4aac9e47
oscar <oscar@InspironOscar>
2023-02-20 20:26:23 +0100

    haciendo commit desde neatbeans por primera vez

--- src/vehiculo/Main.java
+++ src/vehiculo/Main.java
@@ -8,14 +8,20 @@ package vehiculo;
 public class Main {
     
       public static void main(String[] args) {
-        Vehiculo miVehiculo;
+        VehiculoOER2223 miVehiculoOER2223;
         int stockActual;
         
-        miVehiculo = new Vehiculo("Seat",18000,100);
-        try 
+        miVehiculoOER2223 = new VehiculoOER2223("Seat",18000,100);
+        operativaVehiculosOER2223(miVehiculoOER2223, 50); 
+        stockActual = miVehiculoOER2223.obtenerStock();
+        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    public static void operativaVehiculosOER2223(VehiculoOER2223 miVehiculoOER2223, int cantidad) {
+        try
         {
             System.out.println("Venta de Vehiculos");
-            miVehiculo.vender(20);
+            miVehiculoOER2223.vender(20);
         } catch (Exception e)
         {
             System.out.print("Fallo al vender");
@@ -24,13 +30,11 @@ public class Main {
         try
         {
             System.out.println("Compra de Vehiculos");
-            miVehiculo.comprar(100);
+            miVehiculoOER2223.comprar(100);
         } catch (Exception e)
         {
             System.out.print("Fallo al comprar");
         }
-        stockActual = miVehiculo.obtenerStock();
-        System.out.println("El stock actual es"+ stockActual );
     }
 
 }
